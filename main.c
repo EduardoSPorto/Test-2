b119de62b434934c0f518b9cc44fdbaa9d02ba2b
EduardoSPorto <114609263+EduardoSPorto@users.noreply.github.com>
2022-09-28 18:05:03 +0000

    Parte 2, virificar

--- main.c
+++ main.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 
 int main(void) {
-  int op1,ano,idade;
+  int op1,ano,idade,fibo,n1=0,n2=1,c=0;
+  printf("Data a sua idade?");
+  scanf("%d",&idade);  
   printf("Escolha o que voce quer fazer (1)Ano(2)Sequencia\n");
   scanf("%d",&op1);
+
   switch (op1){
     case 1:
       printf("Qual o ano atual?");
-        scanf("%d",ano);
-      printf("Data a sua idade?");
-        scanf("%d",idade);
+        scanf("%d",&ano);
         ano=ano-idade;
       printf("Voce é do ano de %d ",ano);
     break;
     case 2:
-      printf("dois :DDDD");
+      printf("%d-%d",n1,n2);
+      do{
+        c++;
+        fibo=n1+n2;
+        n1=n1+1;
+        n2=n2+1;
+        printf("-%d",fibo);
+      }while(c<idade);
     
     break;
       }
