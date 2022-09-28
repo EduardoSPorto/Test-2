4ffcfadf8b8b3148010e07c4fba881d51fd042b1
EduardoSPorto <114609263+EduardoSPorto@users.noreply.github.com>
2022-09-28 17:49:25 +0000

    Atividade 1

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Ola Mundo\n");
-  printf("And Hello");
+  int op1,ano,idade;
+  printf("Escolha o que voce quer fazer (1)Ano(2)Sequencia\n");
+  scanf("%d",&op1);
+  switch (op1){
+    case 1:
+      printf("Qual o ano atual?");
+        scanf("%d",ano);
+      printf("Data a sua idade?");
+        scanf("%d",idade);
+        ano=ano-idade;
+      printf("Voce é do ano de %d ",ano);
+    break;
+    case 2:
+      printf("dois :DDDD");
+    
+    break;
+      }
   return 0;
 }
